36a0828753c8e311f884412df61412ad86b77d60
noahgill409 <noahgill409@gmail.com>
2022-09-25 07:12:21 -0500

    implement pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 """Package setup."""
 
-import setuptools
-from src.mysql_hello_world.version import Version
+from setuptools import setup
 
-
-setuptools.setup(
-    name="mysql-hello-world",
-    version=Version("1.0.0").number,
-    description="mysql-hello-world",
-    long_description=open("README.md").read().strip(),
-    author="bedevere",
-    author_email="noahgill409@gmail.com",
-    url="https://www.github.com/noahgill409",
-    py_modules=["mysql-hello-world"],
-    install_requires=[],
-    license="MIT License",
-    zip_safe=False,
-    keywords="",
-    classifiers=["Packages"],
-)
+setup()
